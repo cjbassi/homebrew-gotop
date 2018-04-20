d7e2ae5f77e913d1f99b1f158fbd009c329d4150
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-19 20:30:45 -0700

    Brew formula update for gotop version 1.2.13

--- gotop.rb
+++ gotop.rb
@@ -1,9 +1,9 @@
 class Gotop < Formula
   desc "A terminal based graphical activity monitor inspired by gtop and vtop"
   homepage "https://github.com/cjbassi/gotop"
-  url "https://github.com/cjbassi/gotop/releases/download/1.2.12/gotop_1.2.12_darwin_amd64.tgz"
-  version "1.2.12"
-  sha256 "c45c3105fdda3d10510c5ebfbed1e2183f56ec011771f187434d528b073dd5fc"
+  url "https://github.com/cjbassi/gotop/releases/download/1.2.13/gotop_1.2.13_darwin_amd64.tgz"
+  version "1.2.13"
+  sha256 "b6126b8a1c2379259f43dde5c9181650cd990be448701fd4ed0eb0d8f4ccdef9"
 
   def install
     bin.install "gotop"
