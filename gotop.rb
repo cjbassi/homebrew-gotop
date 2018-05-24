fc1992585f55670a85f0150ad28c4be74e41480f
Caleb Bassi <calebjbassi@gmail.com>
2018-05-24 00:08:19 -0700

    Fix sha256

--- gotop.rb
+++ gotop.rb
@@ -3,7 +3,7 @@ class Gotop < Formula
   homepage "https://github.com/cjbassi/gotop"
   url "https://github.com/cjbassi/gotop/releases/download/1.4.0/gotop_1.4.0_darwin_amd64.tgz"
   version "1.4.0"
-  sha256 "b6126b8a1c2379259f43dde5c9181650cd990be448701fd4ed0eb0d8f4ccdef9"
+  sha256 "f56fd2280ff7ef1a9d7170656f64e42d0df189f6e0b644d1ff4bd3e64e40f78e"
 
   def install
     bin.install "gotop"
