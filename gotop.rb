81908044d1898891e37c149ab4991d4477cd96af
Caleb Bassi <calebjbassi@gmail.com>
2019-02-07 01:11:40 -0800

    v2.0.1

--- gotop.rb
+++ gotop.rb
@@ -1,9 +1,9 @@
 class Gotop < Formula
   desc "A terminal based graphical activity monitor inspired by gtop and vtop"
   homepage "https://github.com/cjbassi/gotop"
-  url "https://github.com/cjbassi/gotop/releases/download/2.0.0/gotop_2.0.0_darwin_amd64.tgz"
-  version "2.0.0"
-  sha256 "88e5a525f0857816512e973df78f224a19dadba5a12c4c6f47f00d24a705fce7"
+  url "https://github.com/cjbassi/gotop/releases/download/2.0.1/gotop_2.0.1_darwin_amd64.tgz"
+  version "2.0.1"
+  sha256 "1c6e65dd0a18e54afe3a453190e45cf36572011f476c3ad4713a9056fd7ac87a"
 
   def install
     bin.install "gotop"
